4a8058482b0f436b1cf43de0e69fafd4992c8c64
Omiete Erekosima <erekoomi@gmail.com>
2017-05-22 15:10:43 -0400

    Add files via upload

--- Travel-One/public_html/JS/fancyStuff.js
+++ Travel-One/public_html/JS/fancyStuff.js
@@ -78,4 +78,6 @@ $(document).ready(function(){
             var randVal = minVal+(Math.random()*(maxVal-minVal));
             return typeof floatVal==='undefined'?Math.round(randVal):randVal.toFixed(floatVal);
     }
+    
+
 });
