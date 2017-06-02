633c0c27b523b567b09cf5c8d2dc4348f4e5d858
Omiete Erekosima <erekoomi@gmail.com>
2017-06-02 11:47:40 -0400

    Add files via upload

--- 5th-Ave-Quisine/public_html/JS/fancystuff.js
+++ 5th-Ave-Quisine/public_html/JS/fancystuff.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    //parallax scrolling for the landing and homepage.
-    
-    $(window).scroll(function(){
-        //by offsetting the content by the same value as scrollTop we can
-        //create the illusion that the text is scrolling with its container
-        $('.parallax div').each(function(){
-            $(this).css('margin-top', $(window).scrollTop() - $(this).parent().position().top);
-        });
-        
-        //scroll down when the arrows are clicked
-       /* $('.next').click(function(){
-            
-            $('html, body').animate({
-               scrollTop: $('.mainSection').offset().top 
-            });
-        }); */
+
+$(window).scroll(function(){
+    //by offsetting the content by the same value as scrollTop we can
+    //create the illusion that the text is scrolling with its container
+
+    $('.parallax div').each(function(){
+        $(this).css('margin-top', $(window).scrollTop() - $(this).parent().position().top);
     });
+
+    $('.circle a').click(function(){
+        var $href = $(this).attr('href');
+        $('body').stop().animate({
+            scrollTop: $($href).offset().top
+        },500);
+        return false;
+    });
+    
 });
+
